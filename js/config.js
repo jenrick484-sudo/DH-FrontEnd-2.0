4b31542c2484cd09855a47c8cd92fd6e8694ae08
jenrick484-sudo <montelee282@gmail.com>
2026-05-05 10:21:56 +0800

    url update

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Ito ang URL ng iyong deployed backend. Palitan ito pagkatapos i-deploy.
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://dh-backend-2-0.onrender.com/api';
